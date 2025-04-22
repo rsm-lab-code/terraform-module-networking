f2820dd7f82d9b973d94d65f4f2b1a7417a2b726
mwoldesenbet1 <mwolde617@gmail.com>
2025-04-22 21:53:54 +0000

    updated

--- tgw/outputs.tf
+++ tgw/outputs.tf
@@ -43,7 +43,3 @@ output "tgw_peering_id" {
   value       = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
 }
 
-output "vpc_east_attachment_id" {
-  description = "ID of the us-east-1 VPC attachment"
-  value       = aws_ec2_transit_gateway_vpc_attachment.vpc_east_attachment.id
-}
