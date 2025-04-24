67f3c683b8f6ee66274ec1b77f41795c600d34d7
mwoldesenbet1 <mwolde617@gmail.com>
2025-04-24 16:59:01 +0000

    inspecton

--- inspection_vpc/main.tf
+++ inspection_vpc/main.tf
@@ -86,3 +86,63 @@ resource "aws_nat_gateway" "inspection_nat_gw" {
   
   depends_on = [aws_internet_gateway.inspection_igw]
 }
+
+# Create route table for public subnets
+resource "aws_route_table" "inspection_public_rt" {
+  provider = aws.delegated_account_us-west-2
+  vpc_id   = aws_vpc.inspection_vpc.id
+  
+  tags = {
+    Name        = "inspection-public-rt"
+    Environment = "security"
+    ManagedBy   = "terraform"
+    Tier        = "public"
+  }
+}
+
+# Add default route to internet gateway
+resource "aws_route" "public_internet_route" {
+  provider               = aws.delegated_account_us-west-2
+  route_table_id         = aws_route_table.inspection_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.inspection_igw.id
+}
+
+# Associate public route table with public subnets
+resource "aws_route_table_association" "public_rt_association" {
+  provider       = aws.delegated_account_us-west-2
+  count          = var.public_subnet_count
+  subnet_id      = aws_subnet.inspection_public_subnets[count.index].id
+  route_table_id = aws_route_table.inspection_public_rt.id
+}
+
+# Create route tables for private subnets (one per AZ)
+resource "aws_route_table" "inspection_private_rt" {
+  provider = aws.delegated_account_us-west-2
+  count    = var.private_subnet_count
+  vpc_id   = aws_vpc.inspection_vpc.id
+  
+  tags = {
+    Name        = "inspection-private-rt-${var.az_suffixes[count.index % length(var.az_suffixes)]}"
+    Environment = "security"
+    ManagedBy   = "terraform"
+    Tier        = "private"
+  }
+}
+
+# Add NAT gateway routes to private route tables
+resource "aws_route" "private_nat_route" {
+  provider               = aws.delegated_account_us-west-2
+  count                  = var.private_subnet_count
+  route_table_id         = aws_route_table.inspection_private_rt[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.inspection_nat_gw[count.index % var.public_subnet_count].id
+}
+
+# Associate private route tables with private subnets
+resource "aws_route_table_association" "private_rt_association" {
+  provider       = aws.delegated_account_us-west-2
+  count          = var.private_subnet_count
+  subnet_id      = aws_subnet.inspection_private_subnets[count.index].id
+  route_table_id = aws_route_table.inspection_private_rt[count.index].id
+}
---
+++ inspection_vpc/outputs.tf
@@ -0,0 +1,42 @@
+# inspection_vpc/outputs.tf
+
+output "inspection_vpc_id" {
+  description = "ID of the inspection VPC"
+  value       = aws_vpc.inspection_vpc.id
+}
+
+output "inspection_vpc_cidr" {
+  description = "CIDR block of the inspection VPC"
+  value       = aws_vpc.inspection_vpc.cidr_block
+}
+
+output "inspection_public_subnet_ids" {
+  description = "IDs of public subnets in the inspection VPC (for GWLB endpoints)"
+  value       = aws_subnet.inspection_public_subnets[*].id
+}
+
+output "inspection_private_subnet_ids" {
+  description = "IDs of private subnets in the inspection VPC (for security appliances)"
+  value       = aws_subnet.inspection_private_subnets[*].id
+}
+
+output "inspection_tgw_subnet_ids" {
+  description = "IDs of TGW attachment subnets in the inspection VPC"
+  value       = aws_subnet.inspection_tgw_subnets[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of the NAT gateways in the inspection VPC"
+  value       = aws_nat_gateway.inspection_nat_gw[*].id
+}
+
+output "inspection_public_rt_id" {
+  description = "ID of the public route table in the inspection VPC"
+  value       = aws_route_table.inspection_public_rt.id
+}
+
+output "inspection_private_rt_ids" {
+  description = "IDs of the private route tables in the inspection VPC"
+  value       = aws_route_table.inspection_private_rt[*].id
+}
+
