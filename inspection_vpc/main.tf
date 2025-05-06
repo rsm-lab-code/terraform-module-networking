1186b5789d00dd3611077c05cf0961c8a4e1d1d2
rsm-lab-code <rsm-lab-code@rsmus.com>
2025-05-06 18:19:33 +0000

    update

--- inspection_vpc/main.tf
+++ inspection_vpc/main.tf
@@ -176,24 +176,10 @@ resource "aws_route_table_association" "private_rt_association" {
   route_table_id = aws_route_table.inspection_private_rt[count.index].id
 
  lifecycle {
-    create_before_destroy = true
+    ignore_changes = [route_table_id, subnet_id]
   }
 }
 
-
-# Create Transit Gateway attachment for the inspection VPC
-#resource "aws_ec2_transit_gateway_vpc_attachment" "inspection_vpc_attachment" {
-# provider           = aws.delegated_account_us-west-2
-# subnet_ids         = aws_subnet.inspection_tgw_subnets[*].id
-# transit_gateway_id = var.transit_gateway_id
-# vpc_id             = aws_vpc.inspection_vpc.id
-  
-# appliance_mode_support = "enable"
-  
-# tags = {
-#   Name = "inspection-vpc-tgw-attachment"
-# }
-#}
 
 # Associate the inspection VPC attachment with the TGW route table
 #resource "aws_ec2_transit_gateway_route_table_association" "inspection_vpc_rt_association" {
